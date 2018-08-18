c36ee02337d4fdf75f1310724f0a31518a5da6c8
Reetika Agrawal <reetika.agrawal@walmart.com>
2018-08-18 16:48:17 +0530

    Adding dfs code

--- src/dfs_java/DFS.java
+++ src/dfs_java/DFS.java
@@ -1,17 +1,19 @@
-package dfs_java;
+ package dfs_java;
 
 import bfs_java.BfsOperations;
 
 public class DFS {
 
 	public static void main(String[] args) {
-		DfsOperations obj = new DfsOperations(5);
-		obj.addEdge(1, 2);
-		obj.addEdge(1, 5);
-		obj.addEdge(2, 3);
-		obj.addEdge(3, 4);
+		DfsOperations obj = new DfsOperations(4);
+		obj.addEdge(0, 1);
+	    obj.addEdge(0, 2);
+	    obj.addEdge(1, 2);
+	    obj.addEdge(2, 0);
+	    obj.addEdge(2, 3);
+	    obj.addEdge(3, 3);
 		
-		obj.dfstrack(1);
+		obj.dfstrack(2);
 	}
 
 }
--- src/dfs_java/DfsOperations.java
+++ src/dfs_java/DfsOperations.java
@@ -3,47 +3,59 @@ package dfs_java;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 public class DfsOperations {
 	int[][] mat;
 	int vertex;
-	
-	DfsOperations(int v){
+
+	DfsOperations(int v) {
 		vertex = v;
-		mat = new int[vertex+1][vertex+1];
-		for(int i=0;i<=vertex;i++){
-			for(int j=0;j<=vertex;j++){
-				mat[i][j]=0;
+		mat = new int[vertex][vertex];
+		for (int i = 0; i < vertex; i++) {
+			for (int j = 0; j < vertex; j++) {
+				mat[i][j] = 0;
 			}
 		}
 	}
-	
-	public void addEdge(int v1,int v2){
-		mat[v1][v2]=1;
-		mat[v2][v1]=1;
+
+	public void addEdge(int v1, int v2) {
+		mat[v1][v2] = 1;
 	}
-	
-	public void dfstrack(int start){
-		Queue<Integer> q = new LinkedList<>();
+
+	public void dfstrack(int start) {
+		// print matrix
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				System.out.print(mat[i][j] + "\t");
+			}
+			System.out.println();
+		}
+
+		Stack<Integer> stk = new Stack<>();
 		ArrayList<Integer> visited = new ArrayList<>();
+
+		stk.push(start);
+		visited.add(start);
+		System.out.print(start + "->");
+		int flag=-1;
 		
-		q.add(start);
-		
-		while(q.peek()!=null){
-			int num = q.poll();
-			
-			if(visited.contains(num)){
-				continue;
-			}
-			else{
-				System.out.print(num+"->");
-				visited.add(num);
-			}
-			
-			for(int i=1;i<vertex;i++){
-				if(mat[num][i]==1){
-					q.add(i);
+		while (!stk.empty()) {
+			int num = stk.peek();
+
+			flag=-1;
+			for (int i = 0; i < vertex; i++) {
+				if (mat[num][i] == 1 && !visited.contains(i)) {
+					//System.out.println("pushing:: " + i);
+					stk.push(i);
+					visited.add(i);
+					System.out.print(i + "->");
+					flag=0;
+					break;
 				}
+			}
+			if(flag==-1){
+				stk.pop();
 			}
 		}
 	}
